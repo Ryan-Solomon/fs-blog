9db76b34051ae2705d99467615982f420619850b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-06 20:01:08 -0600

    add Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hi</h1>
+      <Link
+        prefetch={false}
+        href={{
+          pathname: '/[username]',
+          query: { username: 'jeffd23' },
+        }}
+      >
+        <a>Jeff's profile</a>
+      </Link>
     </div>
   );
 }
