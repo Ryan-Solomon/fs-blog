6aa363ac2b6ea701e7ec93ba91e51361de341c27
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-06 19:56:11 -0600

    add admin routes

---
+++ pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export default function AminPostsPage({}) {
+  return (
+    <main>
+      <h1>Admin</h1>
+    </main>
+  );
+}
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,3 +1,7 @@
 export default function Page({}) {
-  return <main></main>;
+  return (
+    <main>
+      <h1>Sign Up</h1>
+    </main>
+  );
 }
